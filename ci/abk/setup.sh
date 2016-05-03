c501d67fd57a3a3e3c22be8e3710043f4abefbb1
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2016-05-03 11:21:06 +0200

    added copy-workspace-plugin to jenkins, added build pipeline for abk

--- ci/abk/setup.sh
+++ ci/abk/setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-curl -X POST "http://localhost:8081/createItem?name=abk" --data-binary "@config.xml" -H "Content-Type: text/xml"
+curl -X POST "http://localhost:8081/createItem?name=abk" --data-binary "@abk.xml" -H "Content-Type: text/xml"
+curl -X POST "http://localhost:8081/createItem?name=abk-sonar" --data-binary "@abk-sonar.xml" -H "Content-Type: text/xml"
+curl -X POST "http://localhost:8081/createItem?name=abk-pipeline" --data-binary "@abk-pipeline.xml" -H "Content-Type: text/xml"
