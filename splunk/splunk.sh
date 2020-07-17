73f869ef7ff702ff9d1581729c229a8a93025de9
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2020-07-17 14:57:57 +0200

    updates

--- splunk/splunk.sh
+++ splunk/splunk.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 8000:8000 --network=local-sb-network -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=admin1234" --name splunk splunk/splunk:latest
+docker run -d -p 8000:8000 -v /etc/localtime:/etc/localtime:ro --network=local-sb-network -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=admin1234" --name splunk splunk/splunk:latest
--- splunk/to-splunk.sh
+++ splunk/to-splunk.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 docker exec -it -u splunk splunk_splunkforwarder_1 /bin/bash -c "/opt/splunkforwarder/bin/splunk add monitor /logs"
 docker exec -it -u splunk splunk_splunkforwarder_1 /bin/bash -c "/opt/splunkforwarder/bin/splunk add forward-server splunk:9997"
+
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idb/application.log -sourcetype idb-app
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idb/performance.log -sourcetype idb-perf
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idb/action.log -sourcetype idb-act
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idp/application.log -sourcetype idp-app
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idp/performance.log -sourcetype idp-perf
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idp/action.log -sourcetype idp-act
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idx/application.log -sourcetype idx-app
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idx/action.log -sourcetype idx-act
+#/opt/splunkforwarder/bin/splunk add monitor /logs/idx/performance.log -sourcetype idx-perf
