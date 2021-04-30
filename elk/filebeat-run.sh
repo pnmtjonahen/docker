9b14ece89e0dba14ccb09e92933566837f3223c7
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2021-04-30 15:11:38 +0200

    filebeat updates

--- elk/filebeat-run.sh
+++ elk/filebeat-run.sh
@@ -6,5 +6,6 @@ docker run -d \
   --volume="$(pwd)/filebeat/filebeat.docker.yml:/usr/share/filebeat/filebeat.yml:ro" \
   --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
   --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
+  --volume="/var/log/ufw.log:/var/log/host-iptables.log:ro" \
   docker.elastic.co/beats/filebeat:7.12.0 filebeat -e -strict.perms=false \
   -E output.elasticsearch.hosts=["elasticsearch:9200"]
